2163ed47c6bcc4de0c9ad340d7f4e2efab98df0a
meghagavankar1 <meghagavankar@nimapinfotech.com>
2023-10-03 17:55:19 +0530

    logical question and js concept

--- logicJ.js
+++ logicJ.js
@@ -9,3 +9,29 @@ function num() {}
 var num;
 console.log(typeof num); // output will be function because when name is same of function and varibale then function get high priority 
 
+function secLar(arr) {
+    // Remove duplicates from the array using a Set
+    const uniqueArr = [...new Set(arr)];
+
+    if (uniqueArr.length < 2) {
+        // If there are fewer than 2 unique values, there's no second-largest value
+        return undefined;
+    }
+
+    let max = uniqueArr[0];
+    let sec = uniqueArr[0];
+
+    for (let i = 1; i < uniqueArr.length; i++) {
+        if (uniqueArr[i] > max) {
+            sec = max;
+            max = uniqueArr[i];
+        } else if (uniqueArr[i] < max && uniqueArr[i] > sec) {
+            sec = uniqueArr[i];
+        }
+    }
+
+    return sec;
+}
+
+const arr = [12, 5, 70, 55, 20, 70];
+console.log(secLar(arr)); // Output: 55
