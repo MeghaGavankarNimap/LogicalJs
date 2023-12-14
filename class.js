40ce7ef69ba1283cf523c482f161f476982c0de5
meghagavankar1 <meghagavankar@nimapinfotech.com>
2023-12-14 14:14:09 +0530

    added some concept

--- class.js
+++ class.js
@@ -26,3 +26,34 @@ console.log(fruits)
 // splice=(add ele at index,remove ele where added new ele,number of ele)
 
   
+// for convering XXXX
+function myfun(str){
+  let end=str.slice(-4)
+  let start=[]
+  for(let i=0;i<4;i++){
+      start+='X'
+     
+  }
+  return start+end
+  
+}
+
+const str='A1234568'
+console.log(myfun(str))
+
+
+// 
+let one = "NIMAP";
+let two = "INFOT";
+let final=''
+for(let i=0;i<one.length;i++){
+          final+=one[i]
+    for(let j=0;j<two.length;j++){
+      if(one.indexOf(one[i])===two.indexOf(two[j])){
+          final+=two[j]
+        }
+    }
+    
+}
+
+console.log(final)
---
+++ htmlJS/text.js
@@ -0,0 +1,10 @@
+// em ,rem and px
+// em = based on parent eg  if there is a <div> element having font-size: 16px then 1em=16px so child size will be 1rem.
+
+
+// if one of the operand is string then + will use for concatination and if not then will convert to numbers.
+
+// sort:
+// sorting will be only for string but for numbers it will be compairison like using a-b for ascending and b-a for descending.
+
+
